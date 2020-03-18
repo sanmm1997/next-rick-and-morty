9544fcff9fd2766f5711dcc2a1021882401c1ac3
Santiago  Marulanda <smarulanda97@outlook.com>
2020-03-17 22:58:12 -0500

    Implement bootstrap in nextjs
    Is needed create pages/_app.js and import custom css

--- pages/characters/[id].js
+++ pages/characters/[id].js
@@ -1,7 +1,7 @@
 import {getCharacter, getCharacters} from "../../api/api";
 import {useRouter} from "next/router";
 
-const Character = (props) => {
+const Character = ({ character }) => {
     const router = useRouter();
     const id = router.query;
 
@@ -9,7 +9,16 @@ const Character = (props) => {
         <>
             <div className="container">
                 <div className="Cht">
-
+                    <div className="Chts-item">
+                        <div className="Chts-item__pic">
+                            <img src={character.image} alt=""/>
+                        </div>
+                        <div className="Chts-item__detail">
+                            <p>{character.name}</p>
+                            <p>{character.species}</p>
+                            <p>{character.gender}</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
--- pages/characters/index.js
+++ pages/characters/index.js
@@ -5,36 +5,31 @@ const Characters = (props) => {
     return (
         <>
             <div className="container">
-                <div className="Chts">
+                <div className="Chts row">
                     { props.characters.map(character => (
-                        <Link href="/characters/[id]" as={`/characters/${character.id}`} prefetch>
-                            <a>
-                                <div className="Chts-item">
-                                    <div className="Chts-item__pic">
-                                        <img src={character.image} alt=""/>
-                                    </div>
-                                    <div className="Chts-item__detail">
-                                        <p>{character.name}</p>
-                                        <p>{character.species}</p>
-                                        <p>{character.gender}</p>
-                                    </div>
-                                </div>
-                            </a>
-                        </Link>
+                        <div className="Chts-item col-sm-12 col-md-4 col-lg-3">
+                            <Link
+                                key={character.id}
+                                href="/characters/[id]"
+                                as={`/characters/${character.id}`}
+                            >
+                                <a>
+                                        <div className="Chts-item__pic">
+                                            <img src={character.image} alt=""/>
+                                        </div>
+                                        <div className="Chts-item__detail">
+                                            <p>{character.name}</p>
+                                            <p>{character.species}</p>
+                                            <p>{character.gender}</p>
+                                        </div>
+                                </a>
+                            </Link>
+                        </div>
                     ))}
                 </div>
             </div>
             <style jsx>
             {`
-                 .Chts {
-                    display: grid;
-                    grid-gap: 15px;
-                    padding: 15px;
-                    grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
-                 }
-                 .Chts-item {
-                    display: block;
-                 }
                  .Chts-item__detail > p {
                     margin: 2px 0;
                     text-align: center;
